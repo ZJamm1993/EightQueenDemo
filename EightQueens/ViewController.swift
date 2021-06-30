99720e3c945840c65655f324ac8951d82b001d60
zhangjingjian <zhangjianjian@joyy.sg>
2021-06-30 10:10:39 +0800

    Initial Commit

---
+++ EightQueens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EightQueens
+//
+//  Created by zjj on 2021/6/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
